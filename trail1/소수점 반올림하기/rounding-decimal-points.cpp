6bf84ba5a0efef5e26ba93bb634327e2baff8a91
stellacustodis <104292502+stellacustodis@users.noreply.github.com>
2026-09-16 00:12:37 +0900

    [Passed] 소수점 반올림하기 (2ms, 0MB)

---
+++ trail1/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+    cout.precision(1);
+    float a = 25.352;
+    cout << a;
+    return 0;
+}
