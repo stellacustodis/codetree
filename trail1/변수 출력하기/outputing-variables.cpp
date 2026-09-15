867f20219943f9a6148043a98b5fdd68059a934f
stellacustodis <104292502+stellacustodis@users.noreply.github.com>
2026-09-16 00:07:05 +0900

    [Passed] 변수 출력하기 (2ms, 0MB)

---
+++ trail1/변수 출력하기/outputing-variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    char b = 'C';
+    string c = "...";
+    cout << a<< c<<b;
+    return 0;
+}
